a23b1fc2447ac2a98be77dce25c5627fd822c204
zunopark <enzoribery@naver.com>
2019-03-06 00:52:08 +0900

    final

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\웹\heogi_3\django_study_hoegi\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
